a28578750f95fc4bfdbd4f57a695e935c9cf93e5
Jorit Vásconez Gerlach <jorit@vasconezgerlach.de>
2022-02-06 12:14:00 +0100

    Update HTML + add download button + remove js

--- index.php
+++ index.php
@@ -5,25 +5,13 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>YouTube Thumbnail Downloader</title>
   <link rel="stylesheet" href="page.css">
+  <script src="page.js"></script>
 </head>
 <body>
   <form action="">
     <input type="url" placeholder="Paste Video URL" id="url">
     <img id="thumbnail" src="" alt="" id="thumbnail">
-    <input type="submit" value="Download" id="submit">
+    <a download="Your Thumbnail.jpg" id="download">Download</a>
   </form>
-  <script>
-    const url = document.querySelector("#url");
-    const thumbnail = document.querySelector("#thumbnail");
-    const button = document.querySelector(".download-btn");
-    url.onkeyup = ()=>{
-      let urlVal = url.value;
-      if(urlVal.indexOf("https://www.youtube.com/watch?v=") != -1){
-        let urlId = urlVal.split('v=')[1].substring(0, 11);
-        let urlImgId = 'https://img.youtube.com/vi/'+url+'/maxresdefault.jpg';
-        thumbnail.src = urlImgId;
-      }
-    }
-  </script>
 </body>
 </html>
